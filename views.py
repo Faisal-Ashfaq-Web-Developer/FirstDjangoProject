cf85dd363f4f33f1d85bb12263ad6c197f1426dd
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2023-08-09 03:04:39 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+    x = calculate()
+    return render(request, "hello.html", {"name": "Faisal!"})
